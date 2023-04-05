7bfee5173993b1910e447b03010c5fd47de4b0d4
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-04-05 11:59:55 +0600

    authentication/authorization

---
+++ backend/boolean/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BooleanConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'boolean'
---
+++ backend/boolean/views.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from backend.urls import UserSerializer
+
+# Create your views here.
+class RegistrationView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            serializer = UserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        logout(request)
+        return Response({'success': 'User Logged Out'}, status=status.HTTP_200_OK)
