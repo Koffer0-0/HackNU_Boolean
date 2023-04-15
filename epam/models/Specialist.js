060c346ddb687fae62d934cf1ec8d7649e41887b
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-04-16 04:54:11 +0600

    Update Specialist.js

--- epam/models/Specialist.js
+++ epam/models/Specialist.js
@@ -3,7 +3,7 @@ import {Schema, model} from 'mongoose'
 const schema = new Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    role: { type: String, default: 'specialist', required: true },
+    role: { type: String, default: 'specialist', readOnly: true },
 })
 
 export const Specialist = model('Specialist', schema)
