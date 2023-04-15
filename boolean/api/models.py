7ed1c530a11b4508926061c93e6e765700c73bae
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-04-15 15:36:17 +0600

    Update models.py

--- boolean/api/models.py
+++ boolean/api/models.py
@@ -1,76 +1,33 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, role, password=None, **extra_fields):
-        if not email:
-            raise ValueError(_('Email field is required'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, role=role, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(email, 'TechnicalSupportOperator', password, **extra_fields)
-
-
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    ROLE_CHOICES = (
-        ('Client', 'Client'),
-        ('TechnicalSpecialist', 'Technical Specialist'),
-        ('TechnicalSupportOperator', 'Technical Support Operator'),
-    )
-
-    email = models.EmailField(unique=True)
-    name = models.CharField(max_length=100)
-    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
-    is_active = models.BooleanField(default=True)
-
-    groups = models.ManyToManyField(
-        'auth.Group',
-        blank=True,
-        help_text=(
-            'The groups this user belongs to. A user will get all permissions '
-            'granted to each of their groups.'
-        ),
-        related_name="customuser_set",
-        related_query_name="user",
-        verbose_name=('groups'),
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        blank=True,
-        help_text=('Specific permissions for this user.'),
-        related_name="customuser_set",
-        related_query_name="user",
-        verbose_name=('user permissions'),
-    )
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'role']
-
-
-class Company(models.Model):
+class Client(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=20)
-    client = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    username = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=128)
 
+class TechnicalSupportOperator(models.Model):
+    first_name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=128)
 
-class Team(models.Model):
+class TechnicalSpecialistGroup(models.Model):
     name = models.CharField(max_length=100)
-    members = models.ManyToManyField(CustomUser, related_name='team_members')
+    def __str__(self):
+        return self.name
 
+class TechnicalSpecialist(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=128)
+    group = models.ForeignKey(TechnicalSpecialistGroup, on_delete=models.CASCADE, related_name='specialists')
 
-class Task(models.Model):
+class Request(models.Model):
     description = models.TextField()
-    created_by = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='created_tasks')
-    assigned_to = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='assigned_tasks')
+    request_type = models.CharField(max_length=100)
+    assigned_by = models.ForeignKey(TechnicalSupportOperator, on_delete=models.CASCADE, related_name='created_requests')
+    created_by = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='created_requests')
+    assigned_to = models.ForeignKey(TechnicalSpecialist, on_delete=models.CASCADE, related_name='assigned_requests')
     status = models.BooleanField(default=False)
