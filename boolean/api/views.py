00e9410d8053e61600c90304a2e0741c61576c49
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-04-15 15:36:40 +0600

    update the api methods/classes

--- boolean/api/views.py
+++ boolean/api/views.py
@@ -1,55 +1,65 @@
-from django.contrib.auth import authenticate, logout, login
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth import authenticate, get_user_model
+from django.db import transaction
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from boolean.urls import UserSerializer
-class RegistrationView(APIView):
-    permission_classes = [AllowAny]
-
-    def get(self, request):
-        message = {
-            "username": "string (required)",
-            "email": "string (required)",
-            "password": "string (required)",
-            "confirm_password": "string (required)",
-            "first_name": "string (optional)",
-            "last_name": "string (optional)"
-        }
-        return Response(message)
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+from api.models import Client, TechnicalSpecialist, TechnicalSupportOperator, TechnicalSpecialistGroup
+from api.permissions import IsTechnicalSupportOperator
+from api.serializers import ClientSerializer, TechnicalSpecialistSerializer, TechnicalSupportOperatorSerializer, \
+    ClientLoginSerializer, TechnicalSpecialistLoginSerializer, TechnicalSupportOperatorLoginSerializer, \
+    TechnicalSpecialistGroupSerializer
 
 
-class LoginView(APIView):
-    permission_classes = [AllowAny]
-    def get(self, request):
-        message = {
-            "username": "string (required)",
-            "password": "string (required)",
-        }
-        return Response(message)
+class LoginView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ClientLoginSerializer
+
+    def get_serializer_class(self):
+        user = self.request.user
+        if isinstance(user, Client):
+            return ClientLoginSerializer
+        elif isinstance(user, TechnicalSpecialist):
+            return TechnicalSpecialistLoginSerializer
+        elif isinstance(user, TechnicalSupportOperator):
+            return TechnicalSupportOperatorLoginSerializer
+        return super().get_serializer_class()
 
     def post(self, request):
-        username = request.data.get('username')
+        email_or_username = request.data.get('email_or_username')
         password = request.data.get('password')
-        user = authenticate(request, username=username, password=password)
+
+        User = get_user_model()
+
+        try:
+            user = User.objects.get(email=email_or_username)
+        except User.DoesNotExist:
+            try:
+                user = User.objects.get(username=email_or_username)
+            except User.DoesNotExist:
+                return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        user = authenticate(request, username=user.username, password=password)
         if user is not None:
-            login(request, user)
-            serializer = UserSerializer(user)
-            return Response(serializer.data)
+            serializer_class = self.get_serializer_class()
+            return Response(serializer_class(user).data)
         else:
             return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
+class ClientRegistration(generics.CreateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [permissions.AllowAny]
 
-class LogoutView(APIView):
-    permission_classes = [IsAuthenticated]
-    def post(self, request):
-        logout(request)
-        return Response({'success': 'User Logged Out'}, status=status.HTTP_200_OK)
+class TechnicalSpecialistRegistration(generics.CreateAPIView):
+    queryset = TechnicalSpecialist.objects.all()
+    serializer_class = TechnicalSpecialistSerializer
+    permission_classes = [permissions.AllowAny]
+
+class TechnicalSpecialistGroupRegistration(generics.CreateAPIView):
+    queryset = TechnicalSpecialist.objects.all()
+    serializer_class = TechnicalSpecialistGroupSerializer
+    permission_classes = [permissions.AllowAny]
+
+class TechnicalSupportOperatorRegistration(generics.CreateAPIView):
+    queryset = TechnicalSupportOperator.objects.all()
+    serializer_class = TechnicalSupportOperatorSerializer
+    permission_classes = [IsTechnicalSupportOperator]
