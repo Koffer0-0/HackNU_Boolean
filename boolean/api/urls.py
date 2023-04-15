3eca52e1abf5d9b5644875f675b9ee3e76226227
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-04-15 15:36:26 +0600

    Update urls.py

--- boolean/api/urls.py
+++ boolean/api/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, include
 
-from rest_framework import routers
-
-from .views import *
-
-
+from . import views
 
 urlpatterns = [
-    path('v1/login/', LoginView.as_view(), name='login'),
-    path('v1/register/', RegistrationView.as_view(), name='register'),
-    path('v1/logout/', LogoutView.as_view(), name='logout'),
+    path('register/client/', views.ClientRegistration.as_view(), name='client-register'),
+    path('register/specialist/', views.TechnicalSpecialistRegistration.as_view(), name='technical-specialist-register'),
+    path('register/group/', views.TechnicalSpecialistGroupRegistration.as_view(), name='technical-specialist-register'),
+    path('register/support-operator/', views.TechnicalSupportOperatorRegistration.as_view(), name='technical-support-operator-register'),
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
